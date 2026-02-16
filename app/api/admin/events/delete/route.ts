8d6b5b23dd9a2cf7a964007e4a99069f65d8b0bd
PC <wellitsnoor@gmail.com>
2026-02-16 17:00:18 +0530

    change

--- app/api/admin/events/delete/route.ts
+++ app/api/admin/events/delete/route.ts
@@ -52,6 +52,7 @@ export async function POST(req: Request) {
         Key: `events/${id}.webp`,
       }),
     );
+    
 
     return NextResponse.json({ message: "Event deleted" }, { status: 200 });
   } catch (error) {
